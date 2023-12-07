3e83c3d2420069914c4c3a019263a6e44a886053
Bernardo Pissutti <bernardopissutti@gmail.com>
2023-12-06 22:59:38 -0300

    Melhorando clean code

--- exercicio3/exercicio3.py
+++ exercicio3/exercicio3.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: [int]) -> int:
-        max_sum = current_sum = nums[0]
+        sum = current_sum = nums[0]
     
         for num in nums[1:]:
             current_sum = max(num, current_sum + num)
-            max_sum = max(max_sum, current_sum)
-        
-        return max_sum
+            sum = max(sum, current_sum)
+            
+        return sum
 
 solution = Solution()
-array = [5,4,-1,7,8]
+array = [-2,1,-3,4,-1,2,1,-5,4]
 resultado = solution.maxSubArray(array)
 print(f"resultado: {resultado}")
 
